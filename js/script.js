332d775839080f077f3f4782f99fc63daca7d0f4
KacSzu <szulckacper19@gmail.com>
2023-05-29 10:51:21 +0200

    Mobile nav fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // MAKING MOBILE NAV WORK
 const btnMobileNavEl = document.querySelector(`.btn-mobile-nav`);
-const headerEl = document.querySelector(`.header`)
-btnMobileNavEl.addEventListener(`click`, function(e){
-    headerEl.classList.toggle(`nav-open`)
-})
+const headerEl = document.querySelector(`.header`);
+const mainNavListEl = document.querySelector(`.main-nav-list`);
+
+btnMobileNavEl.addEventListener(`click`, function (e) {
+  headerEl.classList.toggle(`nav-open`);
+});
+
+mainNavListEl.addEventListener(`click`, function (e) {
+//   e.preventDefault();
+  if (e.target.classList.contains(`main-nav-link`)) {
+    headerEl.classList.remove(`nav-open`);
+  }
+});
